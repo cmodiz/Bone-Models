18487e38185cf76ee216b592110a2ceafb464a63
cmodiz <corinna.modiz@gmail.com>
2025-01-21 14:50:14 +1000

    added martonova_load_cases with Healthy load case

---
+++ load_cases/martonova_load_cases.py
@@ -0,0 +1,16 @@
+class Healthy:
+    def __init__(self):
+        # -> gamma_off
+        self.basal_PTH_pulse_min = None
+        # -> gamme_on
+        self.basal_PTH_pulse_max = None
+        # -> tau_off
+        self.basal_PTH_pulse_off_duration = None
+        # -> tau_on
+        self.basal_PTH_pulse_on_duration = None
+        # -> T
+        self.basal_PTH_pulse_period = None
+        # drug
+        self.drug_dose = None
+        self.injection_frequency = None
+
