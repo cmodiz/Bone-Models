2a70863beaf55e6ae62eb52e823574be3946f2ab
cmodiz <corinna.modiz@gmail.com>
2025-01-21 16:06:45 +1000

    added healthy parameters

--- load_cases/martonova_load_cases.py
+++ load_cases/martonova_load_cases.py
@@ -1,13 +1,13 @@
 class Healthy:
     def __init__(self):
         # -> gamma_off
-        self.basal_PTH_pulse_min = None
+        self.basal_PTH_pulse_min = 0.00332
         # -> gamme_on
-        self.basal_PTH_pulse_max = None
+        self.basal_PTH_pulse_max = 0.00276
         # -> tau_off
-        self.basal_PTH_pulse_off_duration = None
+        self.basal_PTH_pulse_off_duration = 6.4
         # -> tau_on
-        self.basal_PTH_pulse_on_duration = None
+        self.basal_PTH_pulse_on_duration = 4.2
         # -> T
         self.basal_PTH_pulse_period = None
         # drug
