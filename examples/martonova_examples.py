68741c07a00469dd614097ae65bc7e534c894814
cmodiz <corinna.modiz@gmail.com>
2025-01-21 16:09:23 +1000

    fixed initialisation of load case

--- examples/martonova_examples.py
+++ examples/martonova_examples.py
@@ -1,6 +1,20 @@
 from models import Martonova_Model
 from load_cases.martonova_load_cases import Healthy
+import matplotlib.pyplot as plt
 
-model = Martonova_Model(Healthy)
+load_case = Healthy()
+model = Martonova_Model(load_case)
 cellular_activity, time = model.calculate_cellular_activity()
-basal_activity, integrated_activity_for_step_increase, basal_cellular_responsiveness = model.calculate_activity_constants(cellular_activity, time)
+basal_activity, basal_integrated_activity, basal_cellular_responsiveness = model.calculate_activity_constants(cellular_activity, time)
+
+print(f'Basal Activity: {basal_activity}')
+print(f'Integrated Activity: {basal_integrated_activity}')
+print(f'Basal Cellular Responsiveness: {basal_cellular_responsiveness}')
+
+plt.figure(figsize=(10, 6))
+plt.plot(time, cellular_activity, label='Cellular Activity')
+plt.xlabel('Time')
+plt.ylabel('Cellular Activity')
+plt.title('Cellular Activity Over Time')
+plt.legend()
+plt.show()
