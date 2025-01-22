9fab4093cc794e176853514a907de9028c0c5b70
cmodiz <corinna.modiz@gmail.com>
2025-01-22 16:15:50 +1000

    added calibration parameters

---
+++ parameters/modiz_parameters.py
@@ -0,0 +1,9 @@
+class Calibration_Parameters:
+    def __init__(self):
+        self.cellular_responsiveness = 0.030259870370592704
+        self.integrated_activity = 0.0007172096391750288
+
+
+class Parameters:
+    def __init__(self):
+        self.calibration_parameters = Calibration_Parameters()
