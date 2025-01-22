1e693779f98d22024b8fe11059a6b84a318c9365
cmodiz <corinna.modiz@gmail.com>
2025-01-22 16:14:31 +1000

    added modiz_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from models.lemaire_model import Lemaire_Model
 from models.pivonka_model import Pivonka_Model
 from models.martonova_model import Martonova_Model
+from models.modiz_model import Modiz_Model
