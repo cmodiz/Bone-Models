fbbeae75dcb8d62660258c2d579b0b2ed3dcd565
cmodiz <corinna.modiz@gmail.com>
2025-01-23 14:59:49 +1000

    inherit parameters from lemaire model

--- parameters/modiz_parameters.py
+++ parameters/modiz_parameters.py
@@ -1,9 +1,13 @@
+from parameters.lemaire_parameters import Parameters as Lemaire_Parameters
+
+
 class Calibration_Parameters:
     def __init__(self):
         self.cellular_responsiveness = 0.030259870370592704
         self.integrated_activity = 0.0007172096391750288
 
 
-class Parameters:
+class Parameters(Lemaire_Parameters):
     def __init__(self):
+        super().__init__()
         self.calibration_parameters = Calibration_Parameters()
