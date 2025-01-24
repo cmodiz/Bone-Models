7f19e1dab89da90c1f9f6c94bd17129b50d844c5
cmodiz <corinna.modiz@gmail.com>
2025-01-24 14:28:19 +1000

    implemented load cases for modiz model and reference lemaire model

--- load_cases/modiz_load_cases.py
+++ load_cases/modiz_load_cases.py
@@ -29,12 +29,31 @@ class Healthy_to_Osteoporosis:
         self.martonova = Osteoporosis()
 
 
+class Healthy_to_Postmenopausal_Osteoporosis:
+    def __init__(self):
+        self.lemaire = Lemaire_Load_Case()
+        self.martonova = Postmenopausal_Osteoporosis()
+
+
+class Healthy_to_Hypercalcemia:
+    def __init__(self):
+        self.lemaire = Lemaire_Load_Case()
+        self.martonova = Hypercalcemia()
+
+
+class Healthy_to_Hypocalcemia:
+    def __init__(self):
+        self.lemaire = Lemaire_Load_Case()
+        self.martonova = Hypocalcemia()
+
+
 class Healthy_to_Glucocorticoid_Induced_Osteoporosis:
     def __init__(self):
         self.lemaire = Lemaire_Load_Case()
         self.martonova = Glucocorticoid_Induced_Osteoporosis()
 
 
+# Load cases for reference Lemaire model
 class Reference_Healthy_to_Hyperparathyroidism:
     def __init__(self):
         self.OBp_injection = 0
@@ -51,5 +70,84 @@ class Reference_Healthy_to_Hyperparathyroidism:
         self.start_time = 20
         self.end_time = 80
 
+
+class Reference_Healthy_to_Osteoporosis:
+    def __init__(self):
+        self.OBp_injection = 0
+        self.OBa_injection = 0
+        self.OCa_injection = 0
+        # -> I_P
+        self.PTH_injection = 0
+        # -> I_O
+        self.OPG_injection = 0
+        # -> I_L
+        self.RANKL_injection = 0
+        self.PTH_elevation = 0.8252796052631578
+        self.start_time = 20
+        self.end_time = 80
+
+
+class Reference_Healthy_to_Postmenopausal_Osteoporosis:
+    def __init__(self):
+        self.OBp_injection = 0
+        self.OBa_injection = 0
+        self.OCa_injection = 0
+        # -> I_P
+        self.PTH_injection = 0
+        # -> I_O
+        self.OPG_injection = 0
+        # -> I_L
+        self.RANKL_injection = 0
+        self.PTH_elevation = 0.9
+        self.start_time = 20
+        self.end_time = 80
+
+
+class Reference_Healthy_to_Hypercalcemia:
+    def __init__(self):
+        self.OBp_injection = 0
+        self.OBa_injection = 0
+        self.OCa_injection = 0
+        # -> I_P
+        self.PTH_injection = 0
+        # -> I_O
+        self.OPG_injection = 0
+        # -> I_L
+        self.RANKL_injection = 0
+        self.PTH_elevation = 0.19098684210526315
+        self.start_time = 20
+        self.end_time = 80
+
+
+class Reference_Healthy_to_Hypocalcemia:
+    def __init__(self):
+        self.OBp_injection = 0
+        self.OBa_injection = 0
+        self.OCa_injection = 0
+        # -> I_P
+        self.PTH_injection = 0
+        # -> I_O
+        self.OPG_injection = 0
+        # -> I_L
+        self.RANKL_injection = 0
+        self.PTH_elevation = 7.3500657894736845
+        self.start_time = 20
+        self.end_time = 80
+
+
+class Reference_Healthy_to_Glucocorticoid_Induced_Osteoporosis:
+    def __init__(self):
+        self.OBp_injection = 0
+        self.OBa_injection = 0
+        self.OCa_injection = 0
+        # -> I_P
+        self.PTH_injection = 0
+        # -> I_O
+        self.OPG_injection = 0
+        # -> I_L
+        self.RANKL_injection = 0
+        self.PTH_elevation = 1.0565131578947367
+        self.start_time = 20
+        self.end_time = 80
 
 
