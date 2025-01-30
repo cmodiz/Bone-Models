c5a465630759741b80fe0025874a6112ad10866e
cmodiz <corinna.modiz@gmail.com>
2025-01-31 09:37:29 +1000

    added docstrings in sphinx reST style

--- parameters/martonova_parameters.py
+++ parameters/martonova_parameters.py
@@ -1,5 +1,51 @@
 class kinematics:
+    """  This class contains the kinematic parameters of the two-state receptor model by Martonova et al.
+
+    The following table provides a mapping between the model parameters
+    and their original names from the publication:
+
+    +------------------+-----------+------------------+
+    | Parameter Name        | Symbol    | Units
+    +==================+===========+==================+
+    | receptor_desensitized | k_1       |    [1/min]
+    | receptor_resensitized | k_{-1}    |    [1/min]
+    | complex_desensitized  | k_2       |    [1/min]
+    | complex_resensitized  | k_{-2}    |      [1/min]
+    | active_complex_binding | k_r       |  [-]
+    | active_complex_unbinding | k_{-r}    |    [-]
+    | inactive_complex_binding | k_d       |    [-]
+    | receptor              | K_1           | [-]
+    | complex               | K_2           | [-]
+    | active_binding_unbinding | K_r        | [nM]
+    | inactive_complex_unbinding | k_{-d}    | [1/h]
+    | inactive_binding_unbinding | K_d       | [nM]
+
+    :param receptor_desensitized: rate of receptor desensitization
+    :type receptor_desensitized: float
+    :param receptor_resensitized: rate of receptor resensitization
+    :type receptor_resensitized: float
+    :param complex_desensitized: rate of complex desensitization
+    :type complex_desensitized: float
+    :param complex_resensitized: rate of complex resensitization
+    :type complex_resensitized: float
+    :param active_complex_binding: rate of active complex binding
+    :type active_complex_binding: float
+    :param active_complex_unbinding: rate of active complex unbinding
+    :type active_complex_unbinding: float
+    :param inactive_complex_binding: rate of inactive complex binding
+    :type inactive_complex_binding: float
+    :param receptor: combined kinetic receptor constant
+    :type receptor: float
+    :param complex: combined kinetic complex constant
+    :type complex: float
+    :param active_binding_unbinding: active binding unbinding constant
+    :type active_binding_unbinding: float
+    :param inactive_complex_unbinding: rate of inactive complex unbinding
+    :type inactive_complex_unbinding: float
+    :param inactive_binding_unbinding: inactive binding unbinding constant
+    :type inactive_binding_unbinding: float """
     def __init__(self):
+        """ Constructor method. """
         # -> k_1
         self.receptor_desensitized = 0.012
         # -> k_{-1}
@@ -27,7 +73,29 @@ class kinematics:
 
 
 class activity:
+    """ This class contains the activity parameters of the two-state receptor model by Martonova et al.
+
+    The following table provides a mapping between the model parameters
+    and their original names from the publication:
+
+    +------------------+-----------+------------------+
+    | Parameter Name        | Symbol    | Units
+    +==================+===========+==================+
+    | active_receptor        | a_1       |    [nM]
+    | active_complex         | a_2       |    [nM]
+    | inactive_complex       | a_3       |    [nM]
+    | inactive_receptor      | a_4       |    [nM]
+
+    :param active_receptor: activity constant of active receptor
+    :type active_receptor: float
+    :param active_complex: activity constant of active complex
+    :type active_complex: float
+    :param inactive_complex: activity constant of inactive complex
+    :type inactive_complex: float
+    :param inactive_receptor: activity constant of inactive receptor
+    :type inactive_receptor: float """
     def __init__(self):
+        """ Constructor method. """
         # -> a_1
         self.active_receptor = 100 * 20
         # -> a_2
@@ -39,7 +107,32 @@ class activity:
 
 
 class basal_PTH_pulse:
+    """ This class contains the basal PTH pulse parameters of the two-state receptor model by Martonova et al.
+
+    The following table provides a mapping between the model parameters
+    and their original names from the publication:
+
+    +------------------+-----------+------------------+
+    | Parameter Name        | Symbol    | Units
+    +==================+===========+==================+
+    | min                   | gamma_off |    [nM]
+    | max                   | gamma_on  |    [nM]
+    | off_duration          | tau_off   |    [min]
+    | on_duration           | tau_on    |    [min]
+    | period                | T         |    [min]
+
+    :param min: concentration of non-pulsatile share of PTH pulse
+    :type min: float
+    :param max: concentration of pulsatile share of PTH pulse
+    :type max: float
+    :param off_duration: duration of non-pulsatile share of PTH pulse
+    :type off_duration: float
+    :param on_duration: duration of pulsatile share of PTH pulse
+    :type on_duration: float
+    :param period: duration of the PTH pulse period (pulse + non-pulse)
+    :type period: float """
     def __init__(self):
+        """ Constructor method. """
         # -> gamma_off
         self.min = None
         # -> gamme_on
@@ -53,7 +146,26 @@ class basal_PTH_pulse:
 
 
 class injected_PTH_pulse:
+    """ This class contains the injected PTH pulse parameters of the two-state receptor model by Martonova et al.
+
+    The following table provides a mapping between the model parameters
+    and their original names from the publication:
+
+    +------------------+-----------+------------------+
+    | Parameter Name        | Symbol    | Units
+    +==================+===========+==================+
+    | min                   | gamma_off |    [nM]
+    | max                   | gamma_on  |    [nM]
+    | on_duration           | tau_on    |    [min]
+
+    :param min: concentration of non-pulsatile share of PTH pulse
+    :type min: float
+    :param max: concentration of pulsatile share of PTH pulse
+    :type max: float
+    :param on_duration: duration of pulsatile share of PTH pulse
+    :type on_duration: float """
     def __init__(self):
+        """ Constructor method. """
         # -> gamma_off
         self.min = None
         # -> gamma_on
@@ -63,7 +175,29 @@ class injected_PTH_pulse:
 
 
 class pharmacokinetics:
+    """ This class contains the pharmacokinetic parameters of the two-state receptor model by Martonova et al. for injected PTH.
+
+    The following table provides a mapping between the model parameters
+    and their original names from the publication:
+
+    +------------------+-----------+------------------+
+    | Parameter Name        | Symbol    | Units
+    +==================+===========+==================+
+    | absorption_rate       | k_a        |    [1/h]
+    | elimination_rate      | k_e        |    [1/h]
+    | volume_of_distribution| V         |    [L]
+    | bioavailability       | F         |    [-]
+
+    :param absorption_rate: rate of absorption of the injected PTH
+    :type absorption_rate: float
+    :param elimination_rate: rate of elimination of the injected PTH
+    :type elimination_rate: float
+    :param volume_of_distribution: volume of distribution of the injected PTH
+    :type volume_of_distribution: float
+    :param bioavailability: bioavailability of the injected PTH
+    :type bioavailability: float """
     def __init__(self):
+        """ Constructor method. """
         # -> ka
         self.absorption_rate = 4.38  # [1/h]
         # -> ke
@@ -75,6 +209,19 @@ class pharmacokinetics:
 
 
 class Parameters:
+    """ This class contains all parameters of the two-state receptor model by Martonova et al.
+
+    :param kinematics: kinematic parameters of the model, see :class:`kinematics` for details
+    :type kinematics: kinematics
+    :param activity: activity parameters of the model, see :class:`activity` for details
+    :type activity: activity
+    :param basal_PTH_pulse: basal PTH pulse parameters of the model, see :class:`basal_PTH_pulse` for details
+    :type basal_PTH_pulse: basal_PTH_pulse
+    :param injected_PTH_pulse: injected PTH pulse parameters of the model, see :class:`injected_PTH_pulse` for details
+    :type injected_PTH_pulse: injected_PTH_pulse
+    :param pharmacokinetics: pharmacokinetic parameters of the model, see :class:`pharmacokinetics` for details
+    :type pharmacokinetics: pharmacokinetics """
+
     def __init__(self):
         self.kinematics = kinematics()
         self.activity = activity()
