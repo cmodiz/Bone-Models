6a209975586eab772110bc7469d74470888775dc
cmodiz <corinna.modiz@gmail.com>
2025-02-06 16:45:57 +1000

    added scheiner model

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.lemaire_model import Lemaire_Model
 from models.pivonka_model import Pivonka_Model
 from models.martonova_model import Martonova_Model
 from models.modiz_model import Modiz_Model
+from models.scheiner_model import Scheiner_Model
