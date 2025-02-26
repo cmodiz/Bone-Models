6d940b987b344f294133f8bf37df8fb3b15d1f4f
cmodiz <corinna.modiz@gmail.com>
2025-02-26 18:16:53 +1000

    fixed bone volume fraction vs bone volume in percentage

--- bone_models/models/martinez_reina_model.py
+++ bone_models/models/martinez_reina_model.py
@@ -9,7 +9,9 @@ class Martinez_Reina_Model(Scheiner_Model):
     def __init__(self, load_case):
         super().__init__(load_case=load_case)
         self.parameters = Martinez_Reina_Parameters()
-        self.initial_guess_root = np.array([6.196390627918603e-004, 5.583931899482344e-004, 8.069635262731931e-004, self.parameters.bone_volume.vascular_pore_fraction, self.parameters.bone_volume.bone_fraction])
+        self.initial_guess_root = np.array([6.196390627918603e-004, 5.583931899482344e-004, 8.069635262731931e-004,
+                                            self.parameters.bone_volume.vascular_pore_fraction,
+                                            self.parameters.bone_volume.bone_fraction])
         self.steady_state = type('', (), {})()
         self.steady_state.OBp = None
         self.steady_state.OBa = None
@@ -18,6 +20,45 @@ class Martinez_Reina_Model(Scheiner_Model):
         self.denosumab_concentration_over_time = None
         self.time_for_denosumab = None
         self.bone_apparent_density = np.array([])
+        self.bone_material_density = np.array([])
+        self.last_update_time = 0
+
+    def solve_bone_cell_population_model(self, tspan):
+        """ Solve the bone cell population model and volume fractions using the ODE system over a given time interval.
+        The initial conditions are set to the steady-state values.
+        This function is overwritten from the source model to add vascular pore volume fraction and bone volume fraction,
+        that are necessary to solve in every time step.
+
+        :param tspan: time span for the ODE solver
+        :type tspan: numpy.ndarray with start and end time
+        :return: solution of the ODE system
+        :rtype: scipy.integrate._ivp.ivp.OdeResult
+        """
+        x0 = self.calculate_steady_state()
+        time_vector = np.array([0])
+        OBp_vector = np.array([x0[0]])
+        OBa_vector = np.array([x0[1]])
+        OCa_vector = np.array([x0[2]])
+        vascular_pore_fraction_vector = np.array([x0[3]])
+        bone_volume_fraction_vector = np.array([x0[4]])
+        self.initialise_ageing_queue(OBa_vector[-1], OCa_vector[-1], bone_volume_fraction_vector[-1]/100)
+        for time in np.arange(tspan[0], tspan[1], 1):
+            solution = solve_ivp(lambda t, x: self.bone_cell_population_model(x, t), [time_vector[-1], time], [OBp_vector[-1], OBa_vector[-1], OCa_vector[-1],
+                                                                                                               vascular_pore_fraction_vector[-1], bone_volume_fraction_vector[-1]])
+            time_vector = np.append(time_vector, solution.t)
+            OBp_vector = np.append(OBp_vector, solution.y[0])
+            OBa_vector = np.append(OBa_vector, solution.y[1])
+            OCa_vector = np.append(OCa_vector, solution.y[2])
+            vascular_pore_fraction_vector = np.append(vascular_pore_fraction_vector, solution.y[3])
+            bone_volume_fraction_vector = np.append(bone_volume_fraction_vector, solution.y[4])
+
+            self.update_ageing_queue(OBa_vector[-1], OCa_vector[-1], bone_volume_fraction_vector[-1]/100)
+            bone_material_density = (1 + (self.parameters.mineralisation.density_mineral - 1) * self.volume_fraction_mineral +
+                        (self.parameters.mineralisation.density_organic - 1) * self.parameters.mineralisation.volume_fraction_organic)
+            bone_apparent_density = bone_material_density * bone_volume_fraction_vector[-1] / 100
+            self.bone_material_density = np.append(self.bone_material_density, bone_material_density)
+            self.bone_apparent_density = np.append(self.bone_apparent_density, bone_apparent_density)
+        return [time_vector, OBp_vector, OBa_vector, OCa_vector, vascular_pore_fraction_vector, bone_volume_fraction_vector]
 
     def calculate_strain_energy_density(self, OBa, OCa, vascular_pore_fraction, bone_volume_fraction, t):
         stress_vector = self.calculate_macroscopic_stress_vector(t)
@@ -30,7 +71,7 @@ class Martinez_Reina_Model(Scheiner_Model):
         return strain_energy_density
 
     def calculate_compliance_matrix(self, OBa, OCa, bone_volume_fraction, t):
-        youngs_modulus = self.calculate_youngs_modulus(OBa, OCa, bone_volume_fraction)
+        youngs_modulus = self.calculate_youngs_modulus(OBa, OCa, bone_volume_fraction, t)
         # Initialize compliance tensors as 6x6 matrices of zeros
         compliance_matrix = np.zeros((6, 6))
         compliance_matrix[0, 0] = 1 / youngs_modulus
@@ -47,40 +88,32 @@ class Martinez_Reina_Model(Scheiner_Model):
         compliance_matrix[5, 5] = (2 + 2 * self.parameters.mechanics.poissons_ratio) / youngs_modulus
         return compliance_matrix
 
-    def calculate_youngs_modulus(self, OBa, OCa, bone_volume_fraction):
-        ash_fraction = self.calculate_ash_fraction(OBa, OCa, bone_volume_fraction)
-        youngs_modulus = 84.37 * ((bone_volume_fraction / 100) ** 2.58) * (ash_fraction ** 2.74)
+    def calculate_youngs_modulus(self, OBa, OCa, bone_volume_fraction, t):
+        ash_fraction = self.calculate_ash_fraction(OBa, OCa, bone_volume_fraction, t)
+        youngs_modulus = 84.37 * ((bone_volume_fraction/100) ** 2.58) * (ash_fraction ** 2.74)
         return youngs_modulus
 
-    def calculate_ash_fraction(self, OBa, OCa, bone_volume_fraction):
-        volume_fraction_mineral = self.calculate_average_mineral_content(OBa, OCa, bone_volume_fraction)
+    def calculate_ash_fraction(self, OBa, OCa, bone_volume_fraction, t):
+        volume_fraction_mineral = self.calculate_average_mineral_content(OBa, OCa, bone_volume_fraction, t)
         ash_fraction = (self.parameters.mineralisation.density_mineral * volume_fraction_mineral /
                         (self.parameters.mineralisation.density_mineral * volume_fraction_mineral +
                          self.parameters.mineralisation.density_organic *
                          self.parameters.mineralisation.volume_fraction_organic))
-        bone_material_density = (1 + (self.parameters.mineralisation.density_mineral - 1) * volume_fraction_mineral +
-                                 (self.parameters.mineralisation.density_organic - 1) * self.parameters.mineralisation.volume_fraction_organic)
-        bone_apparent_density = bone_material_density * self.parameters.bone_volume.bone_fraction / 100
-        self.bone_apparent_density = np.append(self.bone_apparent_density, bone_apparent_density)
+        self.volume_fraction_mineral = volume_fraction_mineral
         return ash_fraction
 
-    def calculate_average_mineral_content(self, OBa, OCa, bone_volume_fraction):
-        # update mineralization queue
-        if self.ageing_queue is None:
-            self.initialise_ageing_queue(OBa, OCa, bone_volume_fraction/100)
-        else:
-            self.update_ageing_queue(OBa, OCa, bone_volume_fraction/100)
+    def calculate_average_mineral_content(self, OBa, OCa, bone_volume_fraction, t):
         sum_mineral_content = 0
         for j in range(len(self.ageing_queue) - 1):
             sum_mineral_content += self.ageing_queue[j] * self.calculate_mineralisation_law(j)
         sum_mineral_content += self.ageing_queue[-1] * self.parameters.mineralisation.maximum_mineral_content
-        average_mineral_content = sum_mineral_content / (bone_volume_fraction / 100)
+        average_mineral_content = sum_mineral_content / (bone_volume_fraction/100)
         return average_mineral_content
 
     def update_ageing_queue(self, OBa, OCa, bone_volume_fraction):
         # update mineralization queue
-        resorbed_bone_fraction = OCa * self.parameters.bone_volume.resorption_rate
-        formed_bone_fraction = OBa * self.parameters.bone_volume.formation_rate
+        resorbed_bone_fraction = OCa * self.parameters.bone_volume.resorption_rate/100
+        formed_bone_fraction = OBa * self.parameters.bone_volume.formation_rate/100
 
         summed_queue_bone_volume_content = 0
         for i in range(len(self.ageing_queue) - 2, int(self.parameters.mineralisation.lag_time), -1):
@@ -97,10 +130,11 @@ class Martinez_Reina_Model(Scheiner_Model):
         self.ageing_queue[0] = formed_bone_fraction
         summed_queue_bone_volume_content += self.ageing_queue[0]
         # The last element of the queue stores the volume needed for all the elements of VFPREV to sum (1-p)=vb
-        self.ageing_queue[-1] = bone_volume_fraction + (formed_bone_fraction - resorbed_bone_fraction) - summed_queue_bone_volume_content
+        self.ageing_queue[-1] = bone_volume_fraction + (
+                    formed_bone_fraction - resorbed_bone_fraction) - summed_queue_bone_volume_content
         if self.ageing_queue[-1] < 1e-13:
             self.ageing_queue[-1] = 0
-            #print('Take care, volume of last element in the queue <0')
+            print('Take care, volume of last element in the queue <0')
         #print('Updated ageing queue.')
         pass
 
@@ -120,10 +154,11 @@ class Martinez_Reina_Model(Scheiner_Model):
             mineral_content = self.parameters.mineralisation.primary_mineral_content * (
                     t - self.parameters.mineralisation.lag_time) / self.parameters.mineralisation.primary_phase_duration
         else:
-            mineral_content = (self.parameters.mineralisation.maximum_mineral_content + (self.parameters.mineralisation.primary_mineral_content
-                                                                                 - self.parameters.mineralisation.maximum_mineral_content) *
-                       np.exp(-self.parameters.mineralisation.rate * (
-                               t - self.parameters.mineralisation.primary_phase_duration - self.parameters.mineralisation.lag_time)))
+            mineral_content = (self.parameters.mineralisation.maximum_mineral_content + (
+                        self.parameters.mineralisation.primary_mineral_content
+                        - self.parameters.mineralisation.maximum_mineral_content) *
+                               np.exp(-self.parameters.mineralisation.rate * (
+                                       t - self.parameters.mineralisation.primary_phase_duration - self.parameters.mineralisation.lag_time)))
         return mineral_content
 
     def calculate_RANKL_concentration(self, OBp, OBa, t):
@@ -138,7 +173,8 @@ class Martinez_Reina_Model(Scheiner_Model):
         :type t: float
         :return: RANKL concentration
         :rtype: float"""
-        denosumab_effect = self.parameters.denosumab.accessibility_factor * self.parameters.binding_constant.RANKL_denosumab * self.calculate_denosumab_concentration(t)
+        denosumab_effect = self.parameters.denosumab.accessibility_factor * self.parameters.binding_constant.RANKL_denosumab * self.calculate_denosumab_concentration(
+            t)
         RANKL_eff = self.calculate_effective_carrying_capacity_RANKL(OBp, OBa, t)
         RANKL_RANK_OPG = RANKL_eff / (1 + self.parameters.binding_constant.RANKL_OPG *
                                       self.calculate_OPG_concentration(OBp, OBa, t) +
@@ -146,29 +182,32 @@ class Martinez_Reina_Model(Scheiner_Model):
         RANKL = RANKL_RANK_OPG * ((self.parameters.production_rate.intrinsic_RANKL +
                                    self.calculate_external_injection_RANKL(t)) /
                                   (self.parameters.production_rate.intrinsic_RANKL +
-                                  self.parameters.degradation_rate.RANKL * RANKL_eff))
+                                   self.parameters.degradation_rate.RANKL * RANKL_eff))
         return RANKL
 
     def calculate_denosumab_concentration(self, t):
         if self.denosumab_concentration_over_time is None:
-                self.solve_for_denosumab_concentration()
+            self.solve_for_denosumab_concentration()
         if t is None or t <= self.load_case.start_denosumab_treatment or t >= self.load_case.end_denosumab_treatment:
             return 0
         else:
             # calculate how many injections were given already
-            number_of_injections_given = np.floor((t - self.load_case.start_denosumab_treatment) / self.load_case.treatment_period)
+            number_of_injections_given = np.floor(
+                (t - self.load_case.start_denosumab_treatment) / self.load_case.treatment_period)
             # translate the current time to the interval [0, treatment_period]
-            time_translation = (t - self.load_case.start_denosumab_treatment - number_of_injections_given * self.load_case.treatment_period)
+            time_translation = (
+                        t - self.load_case.start_denosumab_treatment - number_of_injections_given * self.load_case.treatment_period)
             # find the closest index in the solution of the denosumab ODE
             closest_index = np.argmin(np.abs(self.time_for_denosumab - time_translation))
             # get C_den in ng/ml and calculate it to pmol/L using the molar mass (in ng/mol) of denosumab
-            denosumb_concentration = self.denosumab_concentration_over_time[closest_index] / (10 ** -3) * (1 / (self.parameters.denosumab.molar_mass * 10 ** 12))
-            return denosumb_concentration
+            denosumab_concentration = self.denosumab_concentration_over_time[closest_index]/self.parameters.denosumab.molar_mass * (10 ** 6)
+            return denosumab_concentration
 
     def solve_for_denosumab_concentration(self):
         initial_denosumb_concentration = 0
         t_span = [0, self.load_case.treatment_period]
-        sol = solve_ivp(self.pharmacokinetics_pharmocodynamics_denosumab, t_span, [initial_denosumb_concentration], rtol=1e-10, atol=1e-10, max_step=1)
+        sol = solve_ivp(self.pharmacokinetics_pharmocodynamics_denosumab, t_span, [initial_denosumb_concentration],
+                        rtol=1e-10, atol=1e-10, max_step=1)
         self.denosumab_concentration_over_time = sol.y[0]  # ng/ml
         self.time_for_denosumab = sol.t
         plt.figure()
@@ -179,12 +218,13 @@ class Martinez_Reina_Model(Scheiner_Model):
         print('Denosumab concentration initialized')
 
     def pharmacokinetics_pharmocodynamics_denosumab(self, t, concentration):
-        dCdt = ((self.parameters.denosumab.absorption_rate * (self.load_case.denosumab_dose / (self.parameters.denosumab.volume_central_compartment
-                                                                                              / self.parameters.denosumab.bioavailability) *
-                                                             np.exp(-self.parameters.denosumab.absorption_rate * t)) -
-                (concentration / (self.parameters.denosumab.michaelis_menten_constant + concentration)) *
-                (self.parameters.denosumab.maximum_volume / (self.parameters.denosumab.volume_central_compartment /
-                                                             self.parameters.denosumab.bioavailability))) -
+        dCdt = ((self.parameters.denosumab.absorption_rate * (
+                    self.load_case.denosumab_dose / (self.parameters.denosumab.volume_central_compartment
+                                                     / self.parameters.denosumab.bioavailability) *
+                    np.exp(-self.parameters.denosumab.absorption_rate * t)) -
+                 (concentration / (self.parameters.denosumab.michaelis_menten_constant + concentration)) *
+                 (self.parameters.denosumab.maximum_volume / (self.parameters.denosumab.volume_central_compartment /
+                                                              self.parameters.denosumab.bioavailability))) -
                 self.parameters.denosumab.elimination_rate * concentration)
         return dCdt
 
@@ -199,37 +239,3 @@ class Martinez_Reina_Model(Scheiner_Model):
             return external_injection_RANKL
         else:
             return 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
