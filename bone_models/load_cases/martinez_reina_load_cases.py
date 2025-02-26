a1a4411b4660ca20457f184ae7a0b034ae785be5
cmodiz <corinna.modiz@gmail.com>
2025-02-26 18:21:56 +1000

    implemented load case

--- bone_models/load_cases/martinez_reina_load_cases.py
+++ bone_models/load_cases/martinez_reina_load_cases.py
@@ -5,9 +5,9 @@ class Martinez_Reina_Load_Case:
     """ Load cases of the Martinez-Reina 2019 model """
 
     def __init__(self):
-        self.start_time = 1
-        self.end_time = 1000 + 2*365
-        self.stress_tensor = np.array([[0, 0, 0], [0, 0, 0], [0, 0, -25]]) * 10 ** -3   # [GPa]
+        self.start_time = 0
+        self.end_time = 4000
+        self.stress_tensor = np.array([[0, 0, 0], [0, 0, 0], [0, 0, -5]]) * 10 ** -3   # [GPa]
         self.OBp_injection = 0
         self.OBa_injection = 0
         self.OCa_injection = 0
@@ -20,9 +20,11 @@ class Martinez_Reina_Load_Case:
         self.TGFb_injection = 0
 
         self.start_postmenopausal_osteoporosis = 0
-        self.end_postmenopausal_osteoporosis = 1000 + 2*365
+        self.end_postmenopausal_osteoporosis = 4000
 
-        self.start_denosumab_treatment = self.start_postmenopausal_osteoporosis + 365  # leave 1 year untreated
-        self.end_denosumab_treatment = self.start_postmenopausal_osteoporosis + 365 * 2
-        self.treatment_period = 183   # every 6 months
-        self.denosumab_dose = 60 * (10**6) / 60  # [ng]/kg body weight (60 kg as reference body weight) every 6months
+        # self.start_denosumab_treatment = self.start_postmenopausal_osteoporosis + 365  # leave 1 year untreated
+        # self.end_denosumab_treatment = self.start_postmenopausal_osteoporosis + 365 * 2
+        self.treatment_period = 168   # every 6 months
+        self.start_denosumab_treatment = 365
+        self.end_denosumab_treatment = 4000
+        self.denosumab_dose = 60 * (10**6)  # [ng]/kg body weight (60 kg as reference body weight) every 6months
