3607d2bc1b965f1e0923bc25b96f66f4f6f519f1
cmodiz <corinna.modiz@gmail.com>
2026-05-04 10:32:07 +0200

    added modiz_parameters for modiz mineralisation model

--- bone_models/bone_mineralisation_models/parameters/__init__.py
+++ bone_models/bone_mineralisation_models/parameters/__init__.py
@@ -1,3 +1,4 @@
 from .ruffoni_parameters import Ruffoni_Parameters
+from .modiz_parameters import Modiz_Parameters
 
-__all__ = ['Ruffoni_Parameters']
+__all__ = ['Ruffoni_Parameters','Modiz_Parameters']
