0d6b639b3dd812fc014fade41fb58cf5d03c0165
cmodiz <corinna.modiz@gmail.com>
2026-05-04 13:53:23 +0200

    removed unused utils import as attempt to fix documentation

--- bone_models/bone_mineralisation_models/__init__.py
+++ bone_models/bone_mineralisation_models/__init__.py
@@ -1,6 +1,5 @@
 from . import models
 from . import load_cases
 from . import parameters
-from . import utils
 
-__all__ = ['models', 'load_cases', 'parameters', 'utils']
+__all__ = ['models', 'load_cases', 'parameters']
