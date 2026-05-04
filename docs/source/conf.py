0924681a8f0ef636ef5e875f1fbd6a78f348db33
cmodiz <corinna.modiz@gmail.com>
2026-05-04 11:53:04 +0200

    attempt to fix non-local sphinx documentation by adding requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-print("CONF PATH:", os.path.abspath('../../'))
-print("SYS.PATH:", sys.path[:3])
-# Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
